b563a31c7d5dd73c9d52853c9db2f1306ea79701
Alberto 'JCN-9000 <jcn9000@gmail.com>
2018-02-12 16:44:10 +0100

    Use MY GIT

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -26,8 +26,8 @@ import (
 	"path/filepath"
 
 	"github.com/gorilla/mux"
-	"github.com/izakmarais/reporter"
-	"github.com/izakmarais/reporter/grafana"
+	"github.com/JCN-9000/reporter"
+	"github.com/JCN-9000/reporter/grafana"
 )
 
 var proto = flag.String("proto", "http://", "Grafana Protocol")
