bdca1fcf425a1438d10771e03bc833608a8da3c3
Alberto 'JCN-9000 <jcn9000@gmail.com>
2018-03-07 18:17:12 +0100

    Update main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/JCN-9000/reporter/grafana"
-	"github.com/JCN-9000/reporter/report"
+	"github.com/IzakMarais/reporter/grafana"
+	"github.com/IzakMarais/reporter/report"
 	"github.com/gorilla/mux"
 )
 
