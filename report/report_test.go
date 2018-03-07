b1ed0b2b750d109b3727ec633164e1b4a255b7ef
Izak Marais <izak.marais@vastech.co.za>
2018-03-07 09:55:11 +0200

    Use worker pool to limit the number of concurrent PNG fetches.
    Fetching all available panels concurrently could overwhelm grafana for large dashboards.
    Fixes #40

--- report/report_test.go
+++ report/report_test.go
@@ -18,6 +18,7 @@ package report
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"io/ioutil"
 	"net/url"
@@ -38,7 +39,15 @@ const dashJSON = `
 			 {"Type":"graph", "Id":22}]
 		},
 		{"Panels":
-			[{"Type":"singlestat", "Id":33}]
+			[
+				{"Type":"singlestat", "Id":33},
+				{"Type":"graph", "Id":44},
+				{"Type":"graph", "Id":55},
+				{"Type":"graph", "Id":66},
+				{"Type":"graph", "Id":77},
+				{"Type":"graph", "Id":88},
+				{"Type":"graph", "Id":99}
+			]
 		}]
 	},
 "Meta":
@@ -46,14 +55,16 @@ const dashJSON = `
 }`
 
 type mockGrafanaClient struct {
-	variables url.Values
+	getPanelCallCount int
+	variables         url.Values
 }
 
-func (m mockGrafanaClient) GetDashboard(dashName string) (grafana.Dashboard, error) {
+func (m *mockGrafanaClient) GetDashboard(dashName string) (grafana.Dashboard, error) {
 	return grafana.NewDashboard([]byte(dashJSON), m.variables), nil
 }
 
-func (m mockGrafanaClient) GetPanelPng(p grafana.Panel, dashName string, t grafana.TimeRange) (io.ReadCloser, error) {
+func (m *mockGrafanaClient) GetPanelPng(p grafana.Panel, dashName string, t grafana.TimeRange) (io.ReadCloser, error) {
+	m.getPanelCallCount++
 	return ioutil.NopCloser(bytes.NewBuffer([]byte("Not actually a png"))), nil
 }
 
@@ -61,7 +72,7 @@ func TestReport(t *testing.T) {
 	Convey("When generating a report", t, func() {
 		variables := url.Values{}
 		variables.Add("var-test", "testvarvalue")
-		gClient := mockGrafanaClient{variables}
+		gClient := &mockGrafanaClient{0, variables}
 		rep := new(gClient, "testDash", grafana.TimeRange{"1453206447000", "1453213647000"}, "")
 		defer rep.Clean()
 
@@ -79,11 +90,15 @@ func TestReport(t *testing.T) {
 				So(err, ShouldBeNil)
 			})
 
+			Convey("It shoud call getPanelPng once per panel", func() {
+				So(gClient.getPanelCallCount, ShouldEqual, 9)
+			})
+
 			Convey("It should create one file per panel", func() {
 				f, err := os.Open(rep.imgDirPath())
 				defer f.Close()
 				files, err := f.Readdir(0)
-				So(files, ShouldHaveLength, 3)
+				So(files, ShouldHaveLength, 9)
 				So(err, ShouldBeNil)
 			})
 		})
@@ -116,12 +131,74 @@ func TestReport(t *testing.T) {
 					So(s, ShouldContainSubstring, "image1")
 					So(s, ShouldContainSubstring, "image22")
 					So(s, ShouldContainSubstring, "image33")
-
+					So(s, ShouldContainSubstring, "image44")
+					So(s, ShouldContainSubstring, "image55")
+					So(s, ShouldContainSubstring, "image66")
+					So(s, ShouldContainSubstring, "image77")
+					So(s, ShouldContainSubstring, "image88")
+					So(s, ShouldContainSubstring, "image99")
 				})
 				Convey("and the time range", func() {
 					So(s, ShouldContainSubstring, "Tue Jan 19 12:27:27 UTC 2016")
 					So(s, ShouldContainSubstring, "Tue Jan 19 14:27:27 UTC 2016")
 				})
+			})
+		})
+
+		Convey("Clean() should remove the temporary folder", func() {
+			rep.Clean()
+
+			_, err := os.Stat(rep.tmpDir)
+			So(os.IsNotExist(err), ShouldBeTrue)
+		})
+	})
+
+}
+
+type errClient struct {
+	getPanelCallCount int
+	variables         url.Values
+}
+
+func (e *errClient) GetDashboard(dashName string) (grafana.Dashboard, error) {
+	return grafana.NewDashboard([]byte(dashJSON), e.variables), nil
+}
+
+//Procude an error on the 2nd panel fetched
+func (e *errClient) GetPanelPng(p grafana.Panel, dashName string, t grafana.TimeRange) (io.ReadCloser, error) {
+	e.getPanelCallCount++
+	if e.getPanelCallCount == 2 {
+		return nil, errors.New("The second panel has some problem")
+	}
+	return ioutil.NopCloser(bytes.NewBuffer([]byte("Not actually a png"))), nil
+}
+
+func TestReportErrorHandling(t *testing.T) {
+	Convey("When generating a report where one panels gives an error", t, func() {
+		variables := url.Values{}
+		gClient := &errClient{0, variables}
+		rep := new(gClient, "testDash", grafana.TimeRange{"1453206447000", "1453213647000"}, "")
+		defer rep.Clean()
+
+		Convey("When rendering images", func() {
+			dashboard, _ := gClient.GetDashboard("")
+			err := rep.renderPNGsParallel(dashboard)
+
+			Convey("It shoud call getPanelPng once per panel", func() {
+				So(gClient.getPanelCallCount, ShouldEqual, 9)
+			})
+
+			Convey("It should create one less image file than the total number of panels", func() {
+				f, err := os.Open(rep.imgDirPath())
+				defer f.Close()
+				files, err := f.Readdir(0)
+				So(files, ShouldHaveLength, 8) //one less than the total number of im
+				So(err, ShouldBeNil)
+			})
+
+			Convey("If any panels return errors, renderPNGsParralel should return the error message from one panel", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, "The second panel has some problem")
 			})
 		})
 
