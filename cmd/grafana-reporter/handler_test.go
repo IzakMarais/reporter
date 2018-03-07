8d29ee68433c3997581a4d56de25450bddb83a74
Alberto 'JCN-9000 <jcn9000@gmail.com>
2018-03-07 18:18:14 +0100

    Update handler_test.go

--- cmd/grafana-reporter/handler_test.go
+++ cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/JCN-9000/reporter/grafana"
-	"github.com/JCN-9000/reporter/report"
+	"github.com/IzakMarais/reporter/grafana"
+	"github.com/IzakMarais/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
