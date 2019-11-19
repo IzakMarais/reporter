f793fce66f91175d7b5f5db2fcf2c2f4a3757998
Izak Marais <izakmarais@yahoo.com>
2019-11-19 08:05:05 +0200

    Build version bump.

--- cmd/grafana-reporter/version.go
+++ cmd/grafana-reporter/version.go
@@ -19,5 +19,5 @@ package main
 //When making new release also tag git repo and update Makefile docker-buil job
 const generatedMajor = "2"
 const generatedMinor = "3"
-const generatedRelease = "0"
-const generatedGitHash = "124b9a35302b3e9d4746ec0cf643abfecda8c21d"
+const generatedRelease = "1"
+const generatedGitHash = "6cfc3e220dc4f8fc0175ae0ec8698d35546d8c10"
